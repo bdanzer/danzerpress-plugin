cbcdaa39a690b55fe946a9268dff31bf5d360d69
bdanzer <britt.danzer@gmail.com>
2018-08-30 22:28:06 -0500

    temp fix parallax

--- resources/js/plugin.js
+++ resources/js/plugin.js
@@ -6,6 +6,7 @@ var parallaxImgSection = document.querySelectorAll('.danzerpress-section .danzer
 
 parallaxImgSection.forEach(parallaxImg => {
     document.addEventListener("scroll", () => {
+        console.log(parallaxImg.closest('.danzerpress-section'));
         parallax(parallaxImg, parallaxImg.closest('.danzerpress-section'));
     });
 });
@@ -17,10 +18,24 @@ if (parallaxImgTitle) {
 } 
 
 function parallax(parallaxImg, sectionParent) {
-    var parentOffsetBottom = sectionParent.offsetTop + sectionParent.offsetHeight;
-    console.log(window.pageYOffset);
-    console.log(parentOffsetBottom);
-    if (window.pageYOffset > sectionParent.offsetTop && window.pageYOffset > parentOffsetBottom) {
-        parallaxImg.style.transform = 'translate3d(0, ' + ((window.pageYOffset - parentOffsetBottom) * .2) + 'px, 0)';
+    var sectionParentTop = getElementOffset(sectionParent),
+        parentOffsetBottom = sectionParentTop + sectionParent.offsetHeight;
+        console.log(sectionParentTop);
+    if (window.pageYOffset > sectionParentTop && window.pageYOffset < parentOffsetBottom) {
+        parallaxImg.style.transform = 'translate3d(0, ' + ((window.pageYOffset - sectionParentTop) * .2) + 'px, 0)';
     }
 }
+
+function getElementOffset(el) {
+    let top = 0;
+    let element = el;
+  
+    // Loop through the DOM tree
+    // and add it's parent's offset to get page offset
+    do {
+      top += element.offsetTop || 0;
+      element = element.offsetParent;
+    } while (element);
+  
+    return top;
+  }
