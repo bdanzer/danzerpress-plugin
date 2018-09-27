bd66b3244ddf1e20b1d331e7f2472f2459d78f68
bdanzer <britt.danzer@gmail.com>
2018-09-26 23:07:39 -0500

    added methodextractor to add to filters easier via objects

--- filters/DanzerpressFilters.php
+++ filters/DanzerpressFilters.php
@@ -1,10 +1,20 @@
 <?php
 namespace Danzerpress\filters;
 
-class DanzerpressFilters extends TwigFilters {
+class DanzerpressFilters extends MethodExtractor {
 	public function __construct() 
 	{
 		parent::__construct();
+	}
+
+	public function __que_functions()
+	{
+		add_filter('timber/twig', function($twig) {
+			foreach ($this->functions as $value) {
+				$twig->addFilter(new \Twig_SimpleFilter($value['name'], [$this, $value['name']]));
+			}
+			return $twig;
+		});
 	}
 
 	public function snake($string) 
--- filters/MethodExtractor.php
+++ filters/MethodExtractor.php
@@ -4,19 +4,19 @@ namespace Danzerpress\filters;
 /**
  * Helper class to create twig filters really easy
  */
-class TwigFilters {
-	protected $functions;
+class MethodExtractor {
+    protected $functions;
 
 	public function __construct() 
 	{
-		$this->__que_functions($this->__set_functions());
+        $this->__set_functions();
+        $this->__que_functions();
 	}
 
 	public function __set_functions() {
 		$this->functions = [];
 		$methods = get_class_methods($this);
 		
-
 		foreach ($methods as $method) {
 			if (false === strpos($method, '__')) {
 				$this->functions[] = ['name' => $method];
@@ -24,15 +24,7 @@ class TwigFilters {
 		}
 
 		return $this->functions;
-	}
-
-	public function __que_functions($functions)
-	{
-		add_filter('timber/twig', function($twig) {
-			foreach ($this->functions as $value) {
-				$twig->addFilter(new \Twig_SimpleFilter($value['name'], [$this, $value['name']]));
-			}
-			return $twig;
-		});
-	}
+    }
+    
+    public function __que_functions() {} //Override this
 }
