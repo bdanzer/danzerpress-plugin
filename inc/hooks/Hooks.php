b8f2360a190585ea78cbcb2ce8c47e24bea697a1
bdanzer <britt.danzer@gmail.com>
2019-03-09 13:30:51 -0600

    going to allow third party plugin for svgs

--- inc/hooks/Hooks.php
+++ inc/hooks/Hooks.php
@@ -5,17 +5,10 @@ class Hooks {
     public function __construct() 
     {
         add_filter('danzerpress_chunk_namespaces', [$this, 'add_to_namespaces']);
-        add_filter('upload_mimes', [$this, 'supported_mimes']);
     }
 
     public function add_to_namespaces($namespaces) 
     {
         return array_merge($namespaces, ['Danzerpress\\chunks\\DP\\']);
-    }
-
-    public function supported_mimes($mimes)
-    {
-        $mimes['svg'] = 'image/svg+xml';
-        return $mimes;
     }
 }
